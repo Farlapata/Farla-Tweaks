3bedaed65b32acaeb56356ab9eab643a8cc61cf5
Farlapata <114352595+Farlapata@users.noreply.github.com>
2026-08-18 18:59:25 +0200

    Wire personalized greeting

--- src/FarlaTweaks.App/MainWindow.xaml.cs
+++ src/FarlaTweaks.App/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows;
 using System.Windows.Input;
 
@@ -8,6 +9,18 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        UpdateGreeting();
+    }
+
+    private void UpdateGreeting()
+    {
+        var hour = DateTime.Now.Hour;
+        GreetingText.Text = hour switch
+        {
+            >= 5 and < 12 => "GOOD MORNING, MATHÉO",
+            >= 12 and < 18 => "GOOD AFTERNOON, MATHÉO",
+            _ => "GOOD EVENING, MATHÉO"
+        };
     }
 
     private void WindowDragArea_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
