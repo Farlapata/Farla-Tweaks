ace77487c6a8d4ff27d77a56f9c47d4c57fcc01a
Farlapata <114352595+Farlapata@users.noreply.github.com>
2026-08-19 10:34:56 +0200

    Add settings window logic

---
+++ src/FarlaTweaks.App/SettingsWindow.xaml.cs
@@ -0,0 +1,30 @@
+using System.Windows;
+using System.Windows.Input;
+
+namespace FarlaTweaks.App;
+
+public partial class SettingsWindow : Window
+{
+    public SettingsWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OpenWizardButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var wizard = new SetupWizard { Owner = this };
+        if (wizard.ShowDialog() == true)
+            DialogResult = true;
+    }
+
+    private void DragArea_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ButtonState == MouseButtonState.Pressed)
+            DragMove();
+    }
+
+    private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
